8c7d87c8d1de678463fb25794b2800b0de707053
HomePC <wiewiora.tomasz1@gmail.com>
2016-11-28 17:14:52 +0100

    dunno whats going on scr1

---
+++ fifoC/skr1/producent.c
@@ -0,0 +1,26 @@
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdio.h>
+
+int main()
+{
+    int fd;
+    char * fifoPath = "PIPE";
+    fd = open(fifoPath, O_WRONLY);
+    for(int i = 0; i < 15;i++)
+    {
+        char buf[10];
+        sprintf(buf,"--%d--",i);
+        //fprintf(1, buf,sizeof(buf));
+        //printf("producent\n");
+        write(1, buf,sizeof(buf));
+        //sleep(1);
+    }
+    close(fd);
+
+
+    return 0;
+}
+
